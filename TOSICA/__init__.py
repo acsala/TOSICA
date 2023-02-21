825f7f8539f9c722c63270bededd4cca562bf5cb
Attila Csala <a@csala.me>
2023-02-21 09:35:34 +0100

    update train_weight_path variable

--- TOSICA/__init__.py
+++ TOSICA/__init__.py
@@ -18,7 +18,7 @@ __version__ = version(name)
 
 mask_path = os.getcwd()+'/mask.npy'
 
-def train(adata, gmt_path, pre_weights='', label_name='Celltype',max_g=300,max_gs=300,mask_ratio =0.015, n_unannotated = 1,batch_size=8, embed_dim=48,depth=2,num_heads=4,lr=0.001, epochs= 10, lrf=0.01):
+def train(adata, gmt_path, pre_weights='', label_name='Celltype',max_g=300,max_gs=300,mask_ratio =0.015, n_unannotated = 1,batch_size=8, embed_dim=48,depth=2,num_heads=4,lr=0.001, epochs= 10, lrf=0.01, train_weights_path = ""):
     r"""
     Fit the model with reference data
     Parameters
@@ -66,7 +66,7 @@ def train(adata, gmt_path, pre_weights='', label_name='Celltype',max_g=300,max_g
     """
     fit_model(adata, gmt_path, pre_weights=pre_weights, label_name=label_name,
               max_g=max_g,max_gs=max_gs,mask_ratio=mask_ratio, n_unannotated = n_unannotated,batch_size=batch_size, 
-              embed_dim=embed_dim,depth=depth,num_heads=num_heads,lr=lr, epochs= epochs, lrf=lrf, train_weights = "")
+              embed_dim=embed_dim,depth=depth,num_heads=num_heads,lr=lr, epochs= epochs, lrf=lrf, train_weights = train_weights_path)
 
 
 def pre(adata,model_weight_path,mask_path = mask_path,laten=False,save_att = 'X_att', save_lantent = 'X_lat',n_step=10000,cutoff=0.1,n_unannotated = 1,batch_size=50,embed_dim=48,depth=2,num_heads=4):
